412a847d6e3792bb24753a2ba569ca424cffd932
JudeSamJ <judesamuelsjj@gmail.com>
2024-11-06 08:33:02 +0530

    success

---
+++ trans/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Card from './Card';
+import './index.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='right-container'>
+      <Card />
+      <Card />
+      <Card />
+      </div>
+     
+    </div>
+  );
+};
+
+export default App;
---
+++ trans/src/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './index.css'
+
+const Card = () => {
+  return (
+    <>
+    <div className='card' />
+    <div className='card-top' />
+    <h1>Card </h1>
+    <div className='card-body'>
+    <div> 
+    <p> Card component rendered inside a container </p>
+    </div>
+    </div> 
+    </>
+  )
+}
+
+export default Card
