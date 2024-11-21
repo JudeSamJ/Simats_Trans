8574cd0e9b7aa1d31f81f7e576d705273004d963
JudeSamJ <judesamuelsjj@gmail.com>
2024-11-21 06:34:26 +0530

    success

--- trans/src/jsx/Card.jsx
+++ trans/src/jsx/Card.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
-import '../css/card.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "../css/card.css"; // Optional: Use for styling
 
 const Card = () => {
+  // Get the current date
+  const currentDate = new Date();
+  const formattedDate = currentDate.toLocaleDateString("en-GB", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  });
+
   return (
-    <>
-      <div className='card'>
-        <div className='card-body'>
-          <h5 className='card-title'>Card title</h5>
+    <div className="container">
+      {/* Top Bar */}
+      <div className="top-bar">
+        {formattedDate} <br/>VALID
+      </div>
+
+      {/* Main Content */}
+      <div className="content">
+        <div className="card">
+          <img
+          src="./public/male-svgrepo-com.svg"
+          alt="Icon"
+          className="card-icon"/>
         </div>
       </div>
-    </>
+
+      {/* Bottom Bar */}
+      <div className="bottom-bar">
+        RETTERI 5
+      </div>
+    </div>
   );
 };
 
